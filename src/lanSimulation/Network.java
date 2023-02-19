998604d4621dc29e4cd3b27a5d9ac696aaa6aac1
Victor Guerra Veloso <victorgvbh@gmail.com>
2023-02-19 03:28:21 -0500

    Replace Node creation with proper Node subclass

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -66,10 +66,10 @@ public class Network {
 	public static Network DefaultExample () {
 		Network network = new Network (2);
 
-		Node wsFilip = new Node (Node.WORKSTATION, "Filip");
+		Node wsFilip = new Workstation (Node.WORKSTATION, "Filip");
 		Node n1 = new Node(Node.NODE, "n1");
-		Node wsHans = new Node (Node.WORKSTATION, "Hans");
-		Node prAndy = new Node (Node.PRINTER, "Andy");
+		Node wsHans = new Workstation (Node.WORKSTATION, "Hans");
+		Node prAndy = new Printer (Node.PRINTER, "Andy");
 
 		wsFilip.setNextNode_(n1);
 		n1.setNextNode_(wsHans);
